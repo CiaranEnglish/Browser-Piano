706644bc29b12047a760e5e81f5b04920a009c16
CiaranEnglish <ciaran.english.2020@mumail.ie>
2024-11-25 18:47:31 +0000

    Added function to play note when piano key is clicked

--- browser-piano/src/PianoBody.js
+++ browser-piano/src/PianoBody.js
@@ -2,29 +2,34 @@ import "./PianoBody.css"
 import * as Tone from 'tone';
 
 function PianoBody() {
+    const playNote = (note) => {
+        const synth = new Tone.Synth().toDestination();
+        synth.triggerAttackRelease(note, "8n");
+    };
+
     return (
       <div className="PianoBody">  
 
             <ul class = "piano-keys">
 
 
-                <li class = "key white"><span>a</span></li>
-                <li class = "key black"><span>w</span></li>
-                <li class = "key white"><span>s</span></li>
-                <li class = "key black"><span>e</span></li>
-                <li class = "key white"><span>d</span></li>
-                <li class = "key white"><span>f</span></li>
-                <li class = "key black"><span>t</span></li>
-                <li class = "key white"><span>g</span></li>
-                <li class = "key black"><span>y</span></li>
-                <li class = "key white"><span>h</span></li>
-                <li class = "key black"><span>u</span></li>
-                <li class = "key white"><span>j</span></li>
-                <li class = "key white"><span>k</span></li>
-                <li class = "key black"><span>o</span></li>
-                <li class = "key white"><span>l</span></li>
-                <li class = "key black"><span>p</span></li>
-                <li class = "key white"><span>;</span></li>
+                <li class = "key white" onClick={() => playNote('C4')}><span>a</span></li>
+                <li class = "key black" onClick={() => playNote('C#4')}><span>w</span></li>
+                <li class = "key white" onClick={() => playNote('D4')}><span>s</span></li>
+                <li class = "key black" onClick={() => playNote('D#4')}><span>e</span></li>
+                <li class = "key white" onClick={() => playNote('E4')}><span>d</span></li>
+                <li class = "key white" onClick={() => playNote('F4')}><span>f</span></li>
+                <li class = "key black" onClick={() => playNote('F#4')}><span>t</span></li>
+                <li class = "key white" onClick={() => playNote('G4')}><span>g</span></li>
+                <li class = "key black" onClick={() => playNote('G#4')}><span>y</span></li>
+                <li class = "key white" onClick={() => playNote('A4')}><span>h</span></li>
+                <li class = "key black" onClick={() => playNote('A#4')}><span>u</span></li>
+                <li class = "key white" onClick={() => playNote('B4')}><span>j</span></li>
+                <li class = "key white" onClick={() => playNote('C5')}><span>k</span></li>
+                <li class = "key black" onClick={() => playNote('C#5')}><span>o</span></li>
+                <li class = "key white" onClick={() => playNote('D5')}><span>l</span></li>
+                <li class = "key black" onClick={() => playNote('D#5')}><span>p</span></li>
+                <li class = "key white" onClick={() => playNote('E5')}><span>;</span></li>
 
             </ul>
 
