c511430174724e72367a28fb418a84c160f61fa0
Dmitrij Tihomirov <140463001+Dmitrij-88@users.noreply.github.com>
2024-12-16 16:04:52 +0000

    Update App.js

--- browser-piano/src/App.js
+++ browser-piano/src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Home from "./pages/home";
 import "./PianoBody"
 import PianoBody from './PianoBody';
+import ChangeThemes from './changethemes';
 
 function App() {
   return (
-    <div className="App">  
-         <div className = "navigation-bar">
+    <div className="App">
+      <div className = "navigation-bar">
                <img src = {logo} alt = "piano-logo" className = "piano-logo" />  
-          </div>               
-        <PianoBody/>
+          </div>        
+       
+          <PianoBody/>
+        <ChangeThemes/>
         <Home/>
       </div>  
   );
